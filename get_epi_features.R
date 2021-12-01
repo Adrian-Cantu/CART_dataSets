faa7d08b5e08c4798ece4e885a4b3c12e5c0aee6
Adrian Cantu <garbanyo@gmail.com>
2021-12-01 17:36:09 +0000

    testing epifeature extraction

--- get_epi_features.R
+++ get_epi_features.R
@@ -2,7 +2,7 @@ library(RMySQL)
 library(tidyverse)
 library(hiAnnotator)
 #library(microbenchmark)
-library(doParallel)
+#library(doParallel)
 library(furrr)
 
 dbConn  <- dbConnect(MySQL(), group='specimen_management')
@@ -23,24 +23,38 @@ if( !file.exists('intSites_full_ALL_CLL.rds') ) {
   intSites <- readRDS('intSites_full_ALL_CLL.rds')
 }
 
-# test_sample <- intSites %>% as.data.frame() %>%
-#   filter(GTSP=='GTSP0567') %>% GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE)
+ test_sample <- intSites %>% as.data.frame() %>%
+   filter(GTSP=='GTSP0567') %>% GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE)
 
 
 epigenetic_features_path <- "epi_rds" 
 epi_files <- list.files(epigenetic_features_path)
 names(epi_files) <- epi_files %>% str_remove(., ".rds")
 
-plan(multisession, workers = 6)
-kk <- future_imap(epi_files, function(x,name){
-  epi_curr <- readRDS(file.path("epi_rds", x))
-  p_kk <- getFeatureCounts(intSites, epi_curr, name) %>%
-    as.data.frame() %>%
-    select(!colnames(as.data.frame(intSites)))
-  print(paste0('done with',name))
-  return(p_kk)
+all_names <- unique(intSites$GTSP)
+all_names <- all_names[1:3]
+
+full_table <- lapply(all_names,function(c_gtsp){
+  c_sample <- intSites %>% as.data.frame() %>%
+    filter(GTSP==c_gtsp) %>% GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE)
+  print(paste0('starting work on ',c_gtsp))
+  plan(multisession, workers = 6)
+  kk <- future_imap(epi_files, function(x,name){
+    epi_curr <- readRDS(file.path("epi_rds", x))
+    p_kk <- getFeatureCounts(c_sample, epi_curr, name) %>%
+      as.data.frame() %>%
+      select(!colnames(as.data.frame(c_sample)))
+    print(paste0('--done with',name))
+    return(p_kk)
+  })
+  plan(sequential)
+  epi_field <- Reduce(cbind,kk)
+  epi_field$GTSP <- c_sample$GTSP
+  return(epi_field)
 })
-epi_field <- Reduce(cbind,kk)
-plan(sequential)
+#epi_field <- Reduce(cbind,kk)
+#lj_GTSP <- function(x,y) {left_join(x,y,by='GTSP')}
+#Reduce(left_join,kk)
+#plan(sequential)
 final_data <- cbind(test_sample %>% as.data.frame() ,epi_field)
 saveRDS(final_data,'intSite_plus_epi_data.rds')
